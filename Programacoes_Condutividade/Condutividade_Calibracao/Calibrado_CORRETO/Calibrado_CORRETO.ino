7fd0c1f1be4ccd068dbb4e89fb6f18ff20c9611f
Geovanna <geovanna.fernandes727@gmail.com>
2026-03-13 20:18:22 -0300

    Atualizei as programações de condutividade, agora tem apenas as que serão utilizadas.

---
+++ Programacoes_Condutividade/Condutividade_Calibracao/Calibrado_CORRETO/Calibrado_CORRETO.ino
@@ -0,0 +1,53 @@
+const int pinoSensor = 34;
+const int numLeituras = 30;
+
+float fatorTDS = 0.5;   // fator de conversão EC → TDS
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+float lerSensorFiltrado()
+{
+
+  long soma = 0;
+
+  for(int i = 0; i < numLeituras; i++)
+  {
+    soma += analogRead(pinoSensor);
+    delay(10);
+  }
+
+  float mediaADC = soma / (float)numLeituras;
+
+  return mediaADC;
+}
+
+void loop()
+{
+
+  float leituraADC = lerSensorFiltrado();
+
+  float tensao = leituraADC * (3.3 / 4095.0);
+
+  // Equação de calibração obtida no gráfico
+  float condutividade = (tensao + 0.0572) / 0.0013;
+
+  // Conversão EC → TDS
+  float TDS = condutividade * fatorTDS;
+
+  Serial.print("Tensao: ");
+  Serial.print(tensao);
+  Serial.print(" V");
+
+  Serial.print(" | Condutividade: ");
+  Serial.print(condutividade);
+  Serial.print(" uS/cm");
+
+  Serial.print(" | TDS: ");
+  Serial.print(TDS);
+  Serial.println(" mg/L");
+
+  delay(2000);
+}
